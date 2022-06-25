3d52700796ff8562b2b71ed667505852a05ed0d6
Fabian Salgado <salgado.fabian@gmail.com>
2022-06-25 17:08:54 -0400

    test preview ruta /

--- src/views/drag3.vue
+++ src/views/drag3.vue
@@ -112,6 +112,15 @@ export default {
 	components: {
 		BaseTimer,
 	},
+	mounted() {
+		document.head.innerHTML =
+			document.head.innerHTML +
+			`<meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="When Great Minds Don’t Think Alike" />
+<meta property="og:description"        content="How much does culture influence creative thinking?" />
+<meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />`;
+	},
 	computed: {
 		...mapState(["timer"]),
 		timeLeft() {
@@ -272,15 +281,15 @@ export default {
 			};
 		},
 		mounted() {
-			this.$nextTick(() => {
-				document.head.innerHTML =
-					document.head.innerHTML +
-					`<meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-<meta property="og:type"               content="article" />
-<meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-<meta property="og:description"        content="How much does culture influence creative thinking?" />
-<meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />`;
-			});
+			this.$nextTick(() => {});
+
+			// const tag = document.createElement("meta");
+			// tag.setAttribute("property", "og:url");
+			// tag.setAttribute(
+			// 	"content",
+			// 	"http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+			// );
+			// document.head.appendChild(tag);
 		},
 		generateUUID() {
 			// Public Domain/MIT
